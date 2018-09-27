b720a596987302b298b00a1d7a83b8b2edb998c1
tonyjhuang <huangtonyj91@gmail.com>
2018-09-26 20:47:55 -0700

    Finish vanilla-dom hw

---
+++ W6D4/vanilla-dom/script.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+
+  // adding SF places as list items
+
+  // --- your code here!
+
+  const inputFavorite = (e) => {
+    e.preventDefault();
+
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favoriteValue = favoriteInput.value;
+    favoriteInput.value = "";
+
+    const li = document.createElement("li");
+    li.textContent = favoriteValue;
+    
+    const ul = document.getElementById("sf-places");
+    ul.appendChild(li);
+  };
+
+
+  const favoriteSubmit = document.querySelector(".favorite-submit");
+  favoriteSubmit.addEventListener("click", inputFavorite);
+
+// adding new photos
+
+// --- your code here!
+
+  const showPhotoForm = (e) => {
+    const photoFormDiv = document.querySelector(".photo-form-container");
+    if (photoFormDiv.className === "photo-form-container") {
+      photoFormDiv.className = "photo-form-container hidden";
+    } else {
+      photoFormDiv.className = "photo-form-container";
+    }
+  };
+
+  const photoFormShowButton = document.querySelector(".photo-show-button");
+  photoFormShowButton.addEventListener("click", showPhotoForm);
+
+
+  const handlePhotoSubmit = (e) => {
+    e.preventDefault();
+
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = "";
+
+    const newImg = document.createElement("img");
+    newImg.src = photoUrl;
+
+    const newPhotoLi = document.createElement("li");
+    newPhotoLi.appendChild(newImg);
+
+    const dogPhotosList = document.querySelector(".dog-photos");
+    dogPhotosList.appendChild(newPhotoLi);
+  };
+
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click", handlePhotoSubmit);
+});
+
+
